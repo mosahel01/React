bd7d559673f586f75e28db9b77eef2ff089ba791
mosahel01 <hoywd3465@gmail.com>
2025-05-28 20:13:13 +0530

    les go learn REACT

---
+++ 01Basic/src/Tea.jsx
@@ -0,0 +1,15 @@
+function Tea() {
+  return (
+    <>
+      <div>Hi MOM!</div>
+      <div>Tea and Code</div>
+      <div>This is our Tea App</div>
+
+      <p>Hello Everyone, This is Mos currently learning React</p>
+      <p>Hope you won't find anything useful in here</p>
+      <h3>Thanks of Checking it Out.</h3>
+    </>
+  );
+}
+
+export default Tea;
