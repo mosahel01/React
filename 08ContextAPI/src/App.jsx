98552b87d27aea6226138d4b42ed5e966ff21d3f
mosahel01 <hoywd3465@gmail.com>
2025-06-29 17:40:13 +0530

    Context API

---
+++ 08ContextAPI/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hola</div>
+      <div>Amigo</div>
+      <div>How you doing ? </div>
+    </>
+  );
+}
+
+export default App;
