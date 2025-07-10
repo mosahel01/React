2b8ae61a2b7db25a6fdf577de2ad52b0520d26bd
mosahel01 <hoywd3465@gmail.com>
2025-07-10 14:52:50 +0530

    useContext

--- 08MiniContext/src/components/Login.jsx
+++ 08MiniContext/src/components/Login.jsx
@@ -15,24 +15,22 @@ function Login() {
   };
 
   return (
-    <>
-      <div>
-        <h2>Login</h2>
-        <input
-          type="text"
-          placeholder="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-    </>
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />{" "}
+      <input
+        type="text"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
   );
 }
 
