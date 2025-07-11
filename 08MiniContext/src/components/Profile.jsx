8148d2c94a8856c67793e412bb33c32c2a240b63
mosahel01 <hoywd3465@gmail.com>
2025-07-11 07:26:23 +0530

    miniContext

--- 08MiniContext/src/components/Profile.jsx
+++ 08MiniContext/src/components/Profile.jsx
@@ -3,13 +3,13 @@ import UserContext from "../Context/UserContext";
 
 function Profile() {
   const { user } = useContext(UserContext);
-  if (!user) return <div>Please login</div>;
-  return <div>Welcome {user.username} </div>;
-  //   if (!user) {
-  //     return <div>Please login</div>;
-  //   } else {
-  //     return <div>Welcome {user.username}</div>;
-  //   }
+  // if (!user) return <div>Please login</div>;
+  // return <div>Welcome {user.username} </div>;
+  if (!user) {
+    return <div>Please login</div>;
+  } else {
+    return <div>Welcome {user.username}</div>;
+  }
 }
 
 export default Profile;
