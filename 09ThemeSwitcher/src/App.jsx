675db422bfec7a93e12cb0ae15c67be4b54174bd
mosahel01 <hoywd3465@gmail.com>
2025-07-12 10:30:50 +0530

    add userContext

---
+++ 09ThemeSwitcher/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-wrap item-center min-h-screen">
+      <div className="w-full">
+        <div className="w-full max-w-sm  mx-auto flex justify-center mb-4">
+          Theme Btn
+        </div>
+        <div className="w-full max-w-sm  mx-auto ">Card</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 09ThemeSwitcher/src/Contexts/Theme.js
@@ -0,0 +1,22 @@
+import { useContext, createContext } from "react";
+
+// const ThemeContext = createContext({
+//   themeMode: "light",
+//   darkTheme: () => {},
+//   lightTheme: () => {},
+// });
+
+// export default function useTheme() {
+//   return useContext(ThemeContext);
+// }
+
+// this is more efficient
+export default function useTheme() {
+  return useContext(
+    createContext({
+      themeMode: "light",
+      darkTheme: () => {},
+      lightTheme: () => {},
+    }).Provider
+  );
+}
