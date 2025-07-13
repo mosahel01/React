5d09d5b8f5c0b0703c05c6a6db89ed2547940616
mosahel01 <hoywd3465@gmail.com>
2025-07-13 09:52:30 +0530

    background toggler added

--- 09ThemeSwitcher/src/App.jsx
+++ 09ThemeSwitcher/src/App.jsx
@@ -1,40 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useState, useEffect } from "react";
 import { ThemeProvider } from "./Contexts/Theme";
 import ThemeBtn from "./Component/ThemeBtn";
 import Card from "./Component/Card";
 
 function App() {
-    const [themeMode, setThemeMode] = useState("light");
+  const [themeMode, setThemeMode] = useState("light");
 
-    const lightTheme = () => {
-        setThemeMode("light");
-    };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
 
-    const darkTheme = () => {
-        setThemeMode("dark");
-    };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
 
-    // Change in Theme with JS
-    useEffect(() => {
-        document.querySelector("html").classList.remove("light", "dark");
-        document.querySelector("html").classList.add(themeMode);
-    }, [themeMode]);
+  // actual change in theme
 
-    return (
-        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-            <div className="flex flex-wrap item-center min-h-screen">
-                <div className="w-full">
-                    <div className="w-full max-w-sm  mx-auto flex justify-center mb-4">
-                        <ThemeBtn />
-                    </div>
-                    <div className="w-full max-w-sm  mx-auto ">
-                        <Card />
-                    </div>
-                </div>
-            </div>
-        </ThemeProvider>
-    );
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className='flex flex-wrap min-h-screen items-center'>
+        <div className='w-full'>
+          <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+            <ThemeBtn />
+          </div>
+
+          <div className='w-full max-w-sm mx-auto'>
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
