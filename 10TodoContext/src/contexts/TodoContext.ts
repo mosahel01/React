8261a83be9f892373dfc7198447075f595ffde82
mosahel01 <hoywd3465@gmail.com>
2025-07-13 11:10:56 +0530

    Add useContext to pass todo with context.

--- 10TodoContext/src/contexts/TodoContext.ts
+++ 10TodoContext/src/contexts/TodoContext.ts
@@ -2,20 +2,32 @@ import { createContext, useContext } from "react";
 
 type todoType = {
   id: number;
-  todos: string;
+  todo: string;
   completed: boolean;
 }[];
 
-const TodoContext = createContext<{ todo: todoType }>({
-  todo: [
+type TodoContextType = {
+  todos: todoType;
+  addTodo: (todo: string) => void;
+  updateTodo: (id: number, todo: string) => void;
+  deleteTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+};
+
+export const TodoContext = createContext<TodoContextType>({
+  todos: [
     {
       id: 1,
-      todos: "Todo Msg",
+      todo: "Todo Msg",
       completed: true,
     },
   ],
+  addTodo: (todo): void => {},
+  updateTodo: (id, todo): void => {},
+  deleteTodo: (id): void => {},
+  toggleTodo: (id): void => {},
 });
 
-export const useTodo = (): { todo: todoType } => {
+export const useTodo = (): TodoContextType => {
   return useContext(TodoContext);
 };
